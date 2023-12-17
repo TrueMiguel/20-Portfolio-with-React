f3e558615bb26e0d776f81c8e5ef8bc5553cf1db
Miguel Maldonado <miguel.maldonado5802@gmail.com>
2023-12-17 11:17:35 -0500

    fixed up the Home.jsx file and the components NavTab file. Able to get the page running now.

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App.jsx';
+import './index.css';
+
+// brining in the required import from 'react-route-dom
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// importing the page components
+import Home from './pages/Home.jsx';
+// import Error from './pages/Error.jsx'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// defining the accessible routes and which components respond to which URL. 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
